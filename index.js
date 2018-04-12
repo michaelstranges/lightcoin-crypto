cf13da635a705580bca8e97c93b3a6c1e05d2e17
Michael Stranges <strangesm@gmail.com>
2018-04-12 00:56:36 +0000

    completed exercises setting up withdrawal and deposit transactions ultimately refactoring using gets and sets

--- index.js
+++ index.js
@@ -1,29 +1,76 @@
-let balance = 500.00;
+//let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance(){
+    let balance = 0;
+    this.transactions.forEach(function(trans){
+      balance += trans.value;
+    });
+    return balance;
+
+  }
+
+  addTransaction(transaction){
+    this.transactions.push(transaction);
   }
 
 }
 
+class Transaction {
 
+  constructor(amount, account){
+    this.amount = amount;
+    this.account = account;
+  }
 
+  commit() {
+    this.time = new Date();
+    if(this.isAllowed()){
+      this.account.addTransaction(this);
+    } else {
+      console.log("Insufficient Funds");
+    }
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value(){
+    return this.amount;
+  }
+
+  isAllowed(){
+    return true;
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  get value(){
+    return this.amount*(-1);
+  }
+
+  isAllowed(){
+    return (this.account.balance - this.amount >= 0);
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('billybob');
 
-t1 = new Withdrawal(50.25);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(150.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
